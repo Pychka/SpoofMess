8b69c05623827b9d28db4864ec98b8c861278ef8
Pychka <kronossluga@gmail.com>
2025-12-25 21:27:18 +0500

    add: Unit test for SES

---
+++ SpoofEntranceServiceTest/SessionServiceTests/Data.cs
@@ -0,0 +1,11 @@
+﻿using SpoofEntranceService.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpoofEntranceServiceTest.SessionServiceTests;
+
+public class Data
+{
+    
+}
--- SpoofEntranceServiceTest/SessionServiceTests/SessionServiceTests.cs
+++ SpoofEntranceServiceTest/SessionServiceTests/SessionServiceTests.cs
@@ -1,11 +1,45 @@
-﻿using Moq;
+﻿using CommonObjects.Results;
 using SpoofEntranceService.Models;
 using SpoofEntranceService.ServiceRealizations;
-using SpoofEntranceService.Services;
 
 namespace SpoofEntranceServiceTest.SessionServiceTests;
 
 public class SessionServiceTests
 {
+    [Theory]
+    [MemberData(nameof(GetSessionInvalidInfos))]
+    public void Check_validate_session_info(SessionInfo session, int statusCode)
+    {
+        Result result = SessionService.IsInvalidSession(session);
 
+        Assert.Equal(statusCode, result.StatusCode);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetAgesSessionInfos))]
+    public void Validate_session_creation_time(SessionInfo session, bool realResult)
+    {
+        bool result = SessionService.IsSessionTooNew(session, DateTime.UtcNow);
+
+        Assert.Equal(realResult, result);
+    }
+
+    public static IEnumerable<object[]> GetSessionInvalidInfos()
+    {
+        yield return new object[] { null!, 404 };
+        yield return new object[] { new SessionInfo { IsActive = false }, 400 };
+        yield return new object[] { new SessionInfo { IsDeleted = true }, 400 };
+        yield return new object[] { new SessionInfo { IsActive = false, IsDeleted = true }, 400 };
+        yield return new object[] { new SessionInfo { IsActive = true, IsDeleted = true }, 400 };
+        yield return new object[] { new SessionInfo { IsActive = true, IsDeleted = false }, 200 };
+    }
+
+    public static IEnumerable<object[]> GetAgesSessionInfos()
+    {
+        yield return new object[] { new SessionInfo { CreatedAt = DateTime.UtcNow }, true };
+        yield return new object[] { new SessionInfo { CreatedAt = DateTime.UtcNow.AddDays(-4) }, true };
+        yield return new object[] { new SessionInfo { CreatedAt = DateTime.UtcNow.AddDays(-7) }, true };
+        yield return new object[] { new SessionInfo { CreatedAt = DateTime.UtcNow.AddDays(-8) }, false };
+        yield return new object[] { new SessionInfo { CreatedAt = DateTime.UtcNow.AddDays(-10) }, false };
+    }
 }
