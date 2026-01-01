ab79382967fb3bf26a224699574832486ebe1581
Pychka <kronossluga@gmail.com>
2026-01-01 17:46:24 +0500

    feat: add realization in-memory cache service

---
+++ DataHelpers/ServiceRealizations/Cache/Memory/LocalCacheService.cs
@@ -0,0 +1,32 @@
+﻿using DataHelpers.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DataHelpers.ServiceRealizations.Cache.Memory;
+
+public class LocalCacheService(IMemoryCache cache) : IMemoryCacheService
+{
+    private readonly IMemoryCache _cache = cache;
+
+    public Task Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Delete(string key)
+    {
+        _cache.Remove(key);
+        return Task.CompletedTask;
+    }
+
+    public Task<T?> Get<T>(string key)
+    {
+        _cache.TryGetValue(key, out T? value);
+        return Task.FromResult(value);
+    }
+
+    public Task Save<T>(string key, T value)
+    {
+        _cache.Set(key, value);
+        return Task.CompletedTask;
+    }
+}
