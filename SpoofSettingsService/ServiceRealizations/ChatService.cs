783d2ab760b8391b2efdc1c3cba969fa9f8c8c77
Pychka <kronossluga@gmail.com>
2026-01-01 18:19:26 +0500

    refactor: clean architecture by SRP principle\n\n- Separate validation\n- Separate work with data storages

---
+++ SpoofSettingsService/ServiceRealizations/ChatService.cs
@@ -0,0 +1,76 @@
+﻿using CommonObjects.Requests;
+using CommonObjects.Results;
+using DataHelpers.ServiceRealizations;
+using SpoofSettingsService.Models;
+using SpoofSettingsService.Repositories;
+using SpoofSettingsService.Services;
+using SpoofSettingsService.Setter;
+using SpoofSettingsService.Validators;
+
+namespace SpoofSettingsService.ServiceRealizations;
+
+public class ChatService(ChatRepository chatRepository, UserRepository userRepository, Repository<ChatType, long> chatTypeRepository, ChatValidator chatValidator) : IChatService
+{
+    private readonly ChatValidator _chatValidator = chatValidator;
+    private readonly ChatRepository _chatRepository = chatRepository;
+    private readonly UserRepository _userRepository = userRepository;
+    private readonly Repository<ChatType, long> _chatTypeRepository = chatTypeRepository;
+
+    public async ValueTask<Result> ChangeSettings(ChangeChatSettingsRequest request, long userId)
+    {
+        UserChatResult result = await GetUserAndChat(userId, request.Id);
+        if (!result.Result.Success)
+            return result.Result;
+
+        result.Chat!.Set(request);
+        return Result.OkResult();
+    }
+
+
+    public async ValueTask<Result> CreateChat(CreateChatRequest request, long userId)
+    {
+        User? user = await _userRepository.GetByIdAsync(userId);
+        Result result = UserValidator.Validate(user);
+        if (!result.Success) return result;
+
+        ChatType? chatType = await _chatTypeRepository.GetByIdAsync(request.ChatTypeId);
+        result = _chatValidator.ValidateChatType(chatType);
+        if (!result.Success) return result;
+
+        Chat? repetition = await _chatRepository.GetByUniqueName(request.UniqueName);
+        result = _chatValidator.ValidateHasChatUniqueName(repetition);
+        if (!result.Success) return result;
+
+        DateTime now = DateTime.UtcNow;
+        Chat newChat = new(request.ChatTypeId, user!.Id, request.ChatName, request.IsPublic, request.UniqueName, now, now);
+
+        await _chatRepository.Change(newChat, repetition);
+
+        return Result.OkResult();
+    }
+
+    public async ValueTask<Result> DeleteChat(long chatId, long userId)
+    {
+        UserChatResult result = await GetUserAndChat(userId, chatId);
+        if (!result.Result.Success)
+            return result.Result;
+
+        await _chatRepository.SoftDeleteAsync(result.Chat!);
+        return Result.OkResult();
+    }
+
+    private async Task<UserChatResult> GetUserAndChat(long userId, long chatId)
+    {
+        User? user = await _userRepository.GetByIdAsync(userId);
+
+        Result result = UserValidator.Validate(user);
+        if (!result.Success) return new(null, null, result);
+
+        Chat? chat = await _chatRepository.GetByIdAsync(chatId);
+
+        result = _chatValidator.ValidateChatAndOwner(chat, userId);
+        if (!result.Success) return new(null, null, result);
+
+        return new(user, chat, Result.OkResult());
+    }
+}
--- SpoofSettingsService/ServiceRealizations/UserService.cs
+++ SpoofSettingsService/ServiceRealizations/UserService.cs
@@ -1,50 +1,38 @@
 ﻿using CommonObjects.Requests;
 using CommonObjects.Results;
-using DataHelpers.Services;
 using Microsoft.EntityFrameworkCore;
 using SpoofSettingsService.Models;
+using SpoofSettingsService.Repositories;
 using SpoofSettingsService.Services;
+using SpoofSettingsService.Setter;
 using SpoofSettingsService.Validators;
 
 namespace SpoofSettingsService.ServiceRealizations;
 
-public class UserService(IRepository repository, SpoofSettingsServiceContext context) : IUserService
+public class UserService(UserRepository userRepository) : IUserService
 {
-    private readonly IRepository _repository = repository;
-    private readonly SpoofSettingsServiceContext _context = context;
-    private const string KEY = "user:";
-    private static readonly TimeSpan UserExpiration = TimeSpan.FromMinutes(10);
+    private readonly UserRepository _userRepository = userRepository;
 
     public async ValueTask<Result> ChangeSettings(ChangeUserSettingsRequest request, long userId)
     {
-        User? user = await _repository.GetAsync(KEY + userId,
-            async () => await _context.Users.FirstOrDefaultAsync(x => x.Id == userId),
-            UserExpiration);
+        User? user = await _userRepository.GetByIdAsync(userId);
         Result result = UserValidator.Validate(user);
 
         if (!result.Success) return result;
 
-        user!.Name = request.Name ?? user.Name;
-        user.MonthsBeforeDelete = request.MonthsBeforeDelete ?? user.MonthsBeforeDelete;
-        user.SearchMe = request.SearchMe ?? user.SearchMe;
-        user.ShowMe = request.ShowMe ?? user.ShowMe;
-        user.InviteMe = request.InviteMe ?? user.InviteMe;
-        user.ForwardMessage = request.ForwardMessage ?? user.ForwardMessage;
-
+        user!.Set(request);
         return Result.SuccessResult();
     }
 
     public async ValueTask<Result> Delete(long userId)
     {
-        User? user = await _repository.GetAsync(KEY + userId,
-            async () => await _context.Users.FirstOrDefaultAsync(x => x.Id == userId),
-            UserExpiration);
+        User? user = await _userRepository.GetByIdAsync(userId);
         Result result = UserValidator.Validate(user);
 
         if (!result.Success) return result;
 
         user!.IsDeleted = true;
-        await _repository.ChangeState(_context, KEY + userId, user, EntityState.Deleted, UserExpiration);
+        await _userRepository.SoftDeleteAsync(user);
 
         return Result.SuccessResult();
     }
