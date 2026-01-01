2c3b0fd84340cfdfa8775e7a2868ea900bbca044
Pychka <kronossluga@gmail.com>
2026-01-01 18:09:18 +0500

    feat: add constructors\n\n- Base constructor without params\n- Constructor accepting base fields

--- CommonObjects/Responses/UserAuthorizeResponse.cs
+++ CommonObjects/Responses/UserAuthorizeResponse.cs
@@ -7,4 +7,13 @@ public class UserAuthorizeResponse
     public string AccessToken { get; set; } = null!;
     public string RefreshToken { get; set; } = null!;
     public SessionInfo SessionInfo { get; set; } = null!;
+
+    public UserAuthorizeResponse() { }
+
+    public UserAuthorizeResponse(string accessToken, string refreshToken, SessionInfo sessionInfo)
+    {
+        AccessToken = accessToken;
+        RefreshToken = refreshToken;
+        SessionInfo = sessionInfo;
+    }
 }
