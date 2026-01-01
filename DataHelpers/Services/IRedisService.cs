d5374f6c6c1aa1b2d28ad4a09fb95350cacbf1e8
Pychka <kronossluga@gmail.com>
2026-01-01 17:59:11 +0500

    feat: add redis cache management service

---
+++ DataHelpers/Services/IRedisService.cs
@@ -0,0 +1,12 @@
+﻿namespace DataHelpers.Services;
+
+public interface IRedisService
+{
+    public Task Save<T>(
+        string key, T value);
+
+    public Task Delete(
+        string key);
+
+    public Task<T?> Get<T>(string key);
+}
