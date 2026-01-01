e625caf7255ce46107e486f325ad0ea106a82cd4
Pychka <kronossluga@gmail.com>
2026-01-01 18:07:52 +0500

    feat: add new Requests\n\n- CreateChatRequest\n- ChangeChatSettingsRequest

---
+++ CommonObjects/Requests/ChangeChatSettingsRequest.cs
@@ -0,0 +1,16 @@
+﻿namespace CommonObjects.Requests;
+
+public class ChangeChatSettingsRequest
+{
+    public long Id { get; set; }
+
+    public long? ChatTypeId { get; set; }
+
+    public long? OwnerId { get; set; }
+
+    public string? ChatName { get; set; }
+
+    public bool? IsPublic { get; set; }
+
+    public string? UniqueName { get; set; }
+}
---
+++ CommonObjects/Requests/CreateChatRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace CommonObjects.Requests;
+
+public class CreateChatRequest
+{
+    public long ChatTypeId { get; set; }
+
+    public required string ChatName { get; set; }
+
+    public bool IsPublic { get; set; }
+
+    public required string UniqueName { get; set; }
+}
