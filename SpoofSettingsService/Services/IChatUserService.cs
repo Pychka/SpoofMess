a65ca19ea6ed4d6d1a9588ced810442ad0b92bcf
Pychka <kronossluga@gmail.com>
2026-01-03 22:26:10 +0500

    feat: add interfaces for 2 services ChatUser and RoleType

---
+++ SpoofSettingsService/Services/IChatUserService.cs
@@ -0,0 +1,11 @@
+﻿using CommonObjects.Requests;
+using CommonObjects.Results;
+
+namespace SpoofSettingsService.Services;
+
+public interface IChatUserService
+{
+    public Task<Result> AddMember(AddMemberRequest request, Guid userId);
+    public Task<Result> RemoveMember(DeleteMemberRequest request, Guid userId);
+    public Task<Result> ChangeMemberRights();
+}
---
+++ SpoofSettingsService/Services/IRoleTypeService.cs
@@ -0,0 +1,8 @@
+﻿using SpoofSettingsService.Models;
+
+namespace SpoofSettingsService.Services;
+
+public interface IRoleTypeService
+{
+    public Task<RoleType?> GetRoleById(long roleId);
+}
