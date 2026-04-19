8b7df4a04d7b638281881aac3dd77a0ea2da7232
Pychka <kronossluga@gmail.com>
2026-04-19 12:42:00 +0500

    feat: add UserEventService and implemented

---
+++ SpoofMessageService/Services/Events/IUserEventsService.cs
@@ -0,0 +1,12 @@
+﻿using CommonObjects.DTO;
+
+namespace SpoofMessageService.Services.Events;
+
+public interface IUserEventsService
+{
+    public event UserUpdatedHandler? UserUpdated;
+
+    public delegate void UserUpdatedHandler(UpdateUserInfo user, Guid userId);
+
+    public void NotifyUpdate(UpdateUserInfo updateUser, Guid userId);
+}
