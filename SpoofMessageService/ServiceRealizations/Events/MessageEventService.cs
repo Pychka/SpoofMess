e1d63b8c54a23180d226a3b6412529e9db4e62b8
Pychka <kronossluga@gmail.com>
2026-04-21 12:42:00 +0500

    feat: add MessageEventService and implemented

---
+++ SpoofMessageService/ServiceRealizations/Events/MessageEventService.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.DTO;
+using SpoofMessageService.Models;
+using SpoofMessageService.Services.Events;
+
+namespace SpoofMessageService.ServiceRealizations.Events;
+
+public class MessageEventService : IMessageEventService
+{
+    public event IMessageEventService.MessageRecivedEventHandler? OnMessageRecived;
+
+    public void ReciveMessage(MessageDTO message, Chat chat) =>
+        OnMessageRecived?.Invoke(message, chat);
+}
---
+++ SpoofMessageService/Services/Events/IMessageEventService.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.DTO;
+using SpoofMessageService.Models;
+
+namespace SpoofMessageService.Services.Events;
+
+public interface IMessageEventService
+{
+    public event MessageRecivedEventHandler OnMessageRecived;
+
+    public delegate void MessageRecivedEventHandler(MessageDTO message, Chat chat);
+
+    public void ReciveMessage(MessageDTO message, Chat chat);
+}
