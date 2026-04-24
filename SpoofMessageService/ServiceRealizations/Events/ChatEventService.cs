bde9ad064fb169e1a6349a60a20cf60098d12973
Pychka <kronossluga@gmail.com>
2026-04-24 12:42:00 +0500

    feat: add ChatEventService and implemented

---
+++ SpoofMessageService/ServiceRealizations/Events/ChatEventService.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.Requests.Changes;
+using SpoofMessageService.Models;
+using SpoofMessageService.Services.Events;
+
+namespace SpoofMessageService.ServiceRealizations.Events;
+
+public class ChatEventService : IChatEventService
+{
+    public event IChatEventService.ChatUpdatedHandler? ChatUpdated;
+
+    public void NotifyUpdate(ChangeChatSettingsRequest updateChat, Chat chat) =>
+        ChatUpdated?.Invoke(updateChat, chat);
+}
---
+++ SpoofMessageService/Services/Events/IChatEventService.cs
@@ -0,0 +1,13 @@
+﻿using CommonObjects.Requests.Changes;
+using SpoofMessageService.Models;
+
+namespace SpoofMessageService.Services.Events;
+
+public interface IChatEventService
+{
+    public event ChatUpdatedHandler? ChatUpdated;
+
+    public delegate void ChatUpdatedHandler(ChangeChatSettingsRequest updateChat, Chat chat);
+
+    public void NotifyUpdate(ChangeChatSettingsRequest updateChat, Chat chat);
+}
