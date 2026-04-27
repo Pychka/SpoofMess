4130ca1a3872fab15439ad139eeeb6f579680cb9
Pychka <kronossluga@gmail.com>
2026-04-27 12:44:23 +0500

    feat: add new fields for clients

--- CommonObjects/DTO/ChatUserDTO.cs
+++ CommonObjects/DTO/ChatUserDTO.cs
@@ -7,13 +7,36 @@ namespace CommonObjects.DTO;
 public class ChatUserDTO
 {
     public Guid Id { get; set; }
+
     public int ChatTypeId { get; set; }
-    public string UniqueName { get; set; }
-    public string Name { get; set; }
+
+    public string UniqueName { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    public Guid? FileId { get; set; }
+
+    public string? OriginalFileName { get; set; }
+
+    [JsonIgnore]
+    public Guid? AvatarId { get; set; }
+
+    [NotMapped]
+    public byte[]? ChatAvatarToken { get; set; }
+
+    [NotMapped]
+    public byte[]? ChatAvatarId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public string? Metadata { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
     [NotMapped]
     [JsonIgnore]
     public List<PermissionResult> Rules =>
             string.IsNullOrEmpty(RulesJson) ? [] : JsonSerializer.Deserialize<List<PermissionResult>>(RulesJson) ?? [];
+
     public string RulesJson { get; set; }
 
 }
